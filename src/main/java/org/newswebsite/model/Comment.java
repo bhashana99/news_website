10215e9104a21ecb8604deca8c0214db46b7b5e5
bhashana99 <bhashanachamodya99@gmail.com>
2025-09-29 11:19:18 +0530

    Define Comment model

---
+++ src/main/java/org/newswebsite/model/Comment.java
@@ -0,0 +1,47 @@
+package org.newswebsite.model;
+
+public class Comment {
+    private int id;
+    private int newsId;
+    private String name;
+    private String content;
+
+    public Comment(int id, int newsId, String name, String content) {
+        this.id = id;
+        this.newsId = newsId;
+        this.name = name;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNewsId() {
+        return newsId;
+    }
+
+    public void setNewsId(int newsId) {
+        this.newsId = newsId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
